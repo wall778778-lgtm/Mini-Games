e75c07c6def4747a568dc254942822c626792d49
wall778778-lgtm <wall778778@gmail.com>
2026-08-20 20:51:04 +0300

    Update script.js

--- games/clicker/script.js
+++ games/clicker/script.js
@@ -1,15 +1,28 @@
 let score = 0;
-
+let clicks = 0;
+let cps = 0;
 
 const scoreText = document.getElementById("score");
-
+const cpsText = document.getElementById("cps");
 const button = document.getElementById("clickButton");
 
 
 button.addEventListener("click", () => {
 
     score++;
+    clicks++;
 
     scoreText.textContent = "Score: " + score;
 
 });
+
+
+setInterval(() => {
+
+    cps = clicks;
+
+    cpsText.textContent = "Clicks per second: " + cps;
+
+    clicks = 0;
+
+}, 1000);
